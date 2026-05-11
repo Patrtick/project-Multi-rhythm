73eb78a1690901fd1f1b7096d8de5d87d6d611e9
Patrtick <mak-007@bk.ru>
2026-05-11 21:01:29 +0500

    Ну типо выстроил уровень и экран победы

--- Multi rhythm/Assets/Scripts/UI/LevelProgress.cs
+++ Multi rhythm/Assets/Scripts/UI/LevelProgress.cs
@@ -1,26 +1,51 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class LevelProgress : MonoBehaviour
 {
     [SerializeField] private Manager manager;
     [SerializeField] private Text progressText;
 
+    [SerializeField] private GameObject winPanel;
+
     private float levelDuration;
     private float timer;
+
+    private bool finished;
 
     private void Start()
     {
         levelDuration = manager.GetLevelDuration();
         timer = 0f;
+
+        if (winPanel != null)
+            winPanel.SetActive(false);
     }
 
     private void Update()
     {
+        if (finished)
+            return;
+
         timer += Time.deltaTime;
+
         var percent = GetProgressPercent();
         progressText.text = $"{percent:0}%";
+
+        if (timer >= levelDuration)
+        {
+            FinishLevel();
+        }
+    }
+
+    private void FinishLevel()
+    {
+        finished = true;
+
+        if (winPanel != null)
+            winPanel.SetActive(true);
+
+        Time.timeScale = 0f;
     }
 
     public float GetProgress01()
